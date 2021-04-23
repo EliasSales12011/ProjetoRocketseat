16c963554e9eb83799e7f6a0ccb7806d5a3d2b13
Elias Sales <eliassilva12011@hotmail.com>
2021-04-23 15:39:17 -0300

    Implementacao do dia 5

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
+
 import Routes from "./src/routes";
+import { PlantProps } from "./src/libs/storage";
+
 import { 
   useFonts,
   Jost_400Regular,
@@ -12,6 +16,25 @@ import {
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  useEffect(() => {
+    
+    const subscription = Notifications.addNotificationReceivedListener(
+    async notification => {
+      const data = notification.request.content.data.plant as PlantProps;
+      console.log(data);
+    });
+
+    return () => subscription.remove();
+
+    // async function notifications() {
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log("####### Notificações Agendadas #######");
+    //   console.log(data);
+    // }
+
+    // notifications();
+  }, []);
 
   if(!fontsLoaded) {
     return <AppLoading />
